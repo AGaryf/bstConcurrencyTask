3d574c989cb11f823ad638ab6b27607afafce8e9
abezhovets <art.bezhovets@gmail.com>
2018-11-15 00:38:27 +0300

    delete while(true) because hasNextLine enough(block while until input)

--- src/main/java/com/greendislike/ConsoleReadingThread.java
+++ src/main/java/com/greendislike/ConsoleReadingThread.java
@@ -7,24 +7,17 @@ public class ConsoleReadingThread extends WordsToDigitConverter implements Runna
     @Override
     public void run() {
         Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            synchronized (array) {
-                array.forEach(System.out::println);
-            }
-
-            while (scanner.hasNextLine()) {
-                String newLine = scanner.nextLine();
-                if (!newLine.isEmpty()) {
-                    int result = WordsToDigitConverter.convert(newLine);
-                    if (result != 0) {
-                        synchronized (array) {
-                            array.add(result);
-                        }
-                        System.out.println("Console Reading Thread: " + result + " added");
-                    } else {
-                        System.out.println("Console Reading Thread: wrong input - " + newLine);
+        while (scanner.hasNextLine()) {
+            String newLine = scanner.nextLine();
+            if (!newLine.isEmpty()) {
+                int result = WordsToDigitConverter.convert(newLine);
+                if (result != 0) {
+                    synchronized (array) {
+                        array.add(result);
                     }
+                    System.out.println("Console Reading Thread: " + result + " added");
+                } else {
+                    System.out.println("Console Reading Thread: wrong input - " + newLine);
                 }
             }
         }
